d1ca69fdb85ab909304578c3f50bf4ceb0f4b6d8
sagarbhasme <sagarbhasme29@gmail.com>
2022-01-05 18:34:12 +0530

    forth change

--- src/testing/Testfirst.java
+++ src/testing/Testfirst.java
@@ -5,6 +5,7 @@ public class Testfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world india is my country");
+System.out.println("Hello Java");
 	}
 
 }
