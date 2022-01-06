6303ebd163c9fa509566aefd095d0c3182498ba7
sagarbhasme <sagarbhasme29@gmail.com>
2022-01-06 12:45:16 +0530

    six change

--- src/testing/Testfirst.java
+++ src/testing/Testfirst.java
@@ -5,7 +5,8 @@ public class Testfirst {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hello world india is my country");
-System.out.println("Hello Java is the world india");
+System.out.println("Hello Java is the world india yes");
+
 	}
 
 }
